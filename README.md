1397de3a78d6eb0659dad331ee2acade5849d915
Luciano Silva <luciano.silva@fabamaq.com>
2019-11-27 19:25:45 +0000

    Implemented search options and test disabling

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ CMake Script to automatically (lazily) configure cmake targets from folder layou
 
 ## Root Targets
 
-Root folders and their respective target types are defined by calling the appropriate function for the required target type.
+Root folders and their respective target types are defined by calling de appropriate function for the required target type.
 
 Both **[executables]** and **[libraries]** are supported as explicit targets by using the following cmake functions:
 
@@ -25,17 +25,19 @@ All targets are named by their immediately containing folder.
 
 Target folder self configure by following a small set of sub folder management rules to infer properties about themselves.
 
-The supported meaningful subfolders are as follows:
+The default folder names **cmake** searches for are editable by overwriting the values of predetermined lists.
 
-### **[include]**
-Include folders are interpreted as containing header files mean to be exported to consuming targets.
+### Public Folders **[include]**
+Public source folders are interpreted as containing header files to be exported to consuming targets.
 
 It is equivalent to calling:
 ```cmake
 target_include_directories (<target> PUBLIC <target_path>/include)
 ```
 
-### **[source/src]**
+You can change the default or add additional search folders by setting or appending to the **TREEMAKE_PUBLIC_DIR** list.
+
+### Private Folders **[source/src]**
 Source folders are interpreted as containing headers and source files to be used internally.
 
 It is equivalent to calling:
@@ -45,7 +47,9 @@ target_include_directories (<target> PRIVATE <target_path>/source)
 target_sources (<target> PRIVATE <files_contained_in_source_folder> )
 ```
 
-### **[modules]**
+You can change the default or add additional search folders by setting or appending to the **TREEMAKE_PRIVATE_DIR** list.
+
+### Module Libraries **[modules]**
 When **modules** folders are found, their subdirectories are automatically loaded as static libraries, privately linked to the containing target.
 
 It is equivalent to calling (per contained folder):
@@ -54,6 +58,8 @@ add_library_dir (<path> STATIC)
 
 target_link_libraries (<containing_target> PRIVATE <added_library>)
 ```
+
+You can change the default or add additional search folders by setting or appending to the **TREEMAKE_MODULES_DIR** list.
 
 ### **[test]**
 When **test** folders are found, their subdirectories are automatically loaded as executables, privately linking the containing target.
@@ -65,10 +71,25 @@ add_executable_dir (<path> PRIVATE <containing_targets>)
 set_target_properties(
     <target_name>
     PROPERTIES
-    RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
+    RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/test)
 
 add_test (
     NAME <target_name> 
     COMMAND <target_name>
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/test)
 ```
+
+You can change the default or add additional search folders by setting or appending to the **TREEMAKE_TEST_DIR** list.
+
+---
+
+## Other Settings
+
+### Disable Test Targets
+Test Targets can be disabled by setting the **TREEMAKE_ENABLE_TESTING** list to **OFF**. This option defaults to true.
+
+### Header file Extensions
+Header file extensions can be edited by setting or appending **TREEMAKE_HEADER_EXT** list. This list defaults to "h;hpp;hxx".
+
+### Source file Extensions
+Source file extensions can be edited by setting or appending **TREEMAKE_SOURCE_EXT** list. This list defaults to "c;cpp;cxx".
--- cmake/treemake.cmake
+++ cmake/treemake.cmake
@@ -1,11 +1,19 @@
 cmake_minimum_required (VERSION 3.12)
 
+list (APPEND TREEMAKE_PRIVATE_DIR source src)
+list (APPEND TREEMAKE_PUBLIC_DIR include)
+list (APPEND TREEMAKE_MODULE_DIR modules)
+list (APPEND TREEMAKE_TEST_DIR test)
+list (APPEND TREEMAKE_SOURCE_EXT c cpp cxx)
+list (APPEND TREEMAKE_HEADER_EXT h hpp hxx)
+
+option (TREEMAKE_ENABLE_TESTING "Enable testing targets" TRUE)
+
 function (dir_target_name target_name)
     set (${target_name} ${_current_dir_target_name})
 endfunction()
 
 function (_include_for_target cmake_file target_name)
-    message (STATUS "including file: ${cmake_file}")
     set (_current_target_name ${target_name} PARENT_SCOPE)
     include (${cmake_file})
 endfunction()
@@ -35,27 +43,30 @@ function (_evaluate_path directory path)
     endif ()
 endfunction()
 
-function (_glob_headers glob path headers)
-    file (${glob} values
-        ${path}/*.h
-        ${path}/*.hpp
-        ${path}/*.hxx
-    )
+function (_find_in_path glob extensions path result)
+    set (values "${${result}}")
 
-    set (${headers} ${values} PARENT_SCOPE)
+    foreach (ext ${extensions})
+        file (${glob} file_values ${path}/*.${ext})
+        list(APPEND values ${file_values})
+    endforeach()
+
+    set (${result} ${values} PARENT_SCOPE)
 endfunction()
 
-function (_glob_sources glob path sources)
-    file (${glob} values
-        ${path}/*.c
-        ${path}/*.cpp
-        ${path}/*.cxx
-    )
+function (_find_in_dirs glob extensions path subdirs result)
+    set (values "${${result}}")
 
-    set (${sources} ${values} PARENT_SCOPE)
+    foreach (dir ${subdirs})
+        _find_in_path (${glob} "${extensions}" ${path}/${dir} file_values)
+        list(APPEND values ${file_values})
+    endforeach()
+
+    set (${result} ${values} PARENT_SCOPE)
 endfunction()
 
 function (_add_target_dir_test target_name path)
+
     # enable loading executables as libraries
     set_target_properties(
         ${target_name}
@@ -64,17 +75,16 @@ function (_add_target_dir_test target_name path)
     )
 
     # is it "short test target"
-    _glob_sources (GLOB ${path}/test test_source_files)
+    _find_in_path (GLOB "${TREEMAKE_SOURCE_EXT}" ${path} test_source_files)
     list(LENGTH test_source_files test_source_files_count)
 
     if (test_source_files_count EQUAL 0)
-        _get_directories (${path}/test tests)
+        _get_directories (${path} tests)
 
         foreach (test ${tests})
             add_executable_dir (
                 ${test}
-                PUBLIC ${_public_test_link_libraries}
-                PRIVATE ${_private_test_link_libraries} ${target_name}
+                PRIVATE ${target_name}
             )
         
             get_filename_component (test_name ${test} NAME)
@@ -89,14 +99,14 @@ function (_add_target_dir_test target_name path)
 
         endforeach()
     else ()
+
         set (test_name ${target_name}_test)
         add_executable (${test_name})
 
         _add_short_target_dir (
             ${test_name}
-            ${path}/test
-            PUBLIC ${_public_test_link_libraries}
-            PRIVATE ${_private_test_link_libraries} ${target_name}
+            ${path}
+            PRIVATE ${target_name}
         )
 
         set_target_properties(
@@ -110,6 +120,7 @@ function (_add_target_dir_test target_name path)
 endfunction ()
 
 function (_add_short_target_dir target_name path)
+
     # evaluate arguments
     set(options "")
     set(values "")
@@ -119,11 +130,20 @@ function (_add_short_target_dir target_name path)
 
     target_link_libraries (${target_name} PUBLIC ${LINK_PUBLIC} PRIVATE ${LINK_PRIVATE})
 
-    _glob_headers (GLOB_RECURSE ${path} private_headers)
-    _glob_sources (GLOB_RECURSE ${path} source_files)
+    _find_in_path (
+            GLOB_RECURSE
+            "${TREEMAKE_HEADER_EXT}"
+            ${path}
+            header_files)
+
+    _find_in_path (
+            GLOB_RECURSE
+            "${TREEMAKE_SOURCE_EXT}"
+            ${path}
+            source_files)
 
     target_include_directories (${target_name} PRIVATE ${path})
-    target_sources (${target_name} PRIVATE ${private_headers} ${source_files})
+    target_sources (${target_name} PRIVATE ${header_files} ${source_files})
 
     # include cmakelists
     if (EXISTS ${path}/CMakeLists.txt)
@@ -132,6 +152,7 @@ function (_add_short_target_dir target_name path)
 endfunction ()
 
 function (_add_target_dir target_name path)
+
     # evaluate arguments
     set(options "")
     set(values "")
@@ -141,45 +162,73 @@ function (_add_target_dir target_name path)
 
     target_link_libraries (${target_name} PUBLIC ${LINK_PUBLIC} PRIVATE ${LINK_PRIVATE})
 
-    # read public include folder
-    if (EXISTS ${path}/include)
-        set (public_header_path ${path}/include)
-        _glob_headers (GLOB_RECURSE ${public_header_path} public_headers)
+    _find_in_dirs (
+            GLOB_RECURSE
+            "${TREEMAKE_HEADER_EXT}"
+            ${path}
+            "${TREEMAKE_PUBLIC_DIR}"
+            public_headers)
 
-        target_include_directories (${target_name} PUBLIC ${public_header_path})
+    list(LENGTH public_headers public_header_count)
+
+    if (NOT public_header_count EQUAL 0)
         target_sources (${target_name} PUBLIC ${public_headers})
-    endif()
 
-    # read source files and private include folder
-    if (EXISTS ${path}/source)
-        set (source_path ${path}/source)
-    elseif(EXISTS ${path}/src)
-        set (source_path ${path}/src)
-    endif ()
-
-    if (source_path)
-        _glob_headers (GLOB_RECURSE ${source_path} private_headers)
-        _glob_sources (GLOB_RECURSE ${source_path} source_files)
-
-        target_include_directories (${target_name} PRIVATE ${source_path})
-        target_sources (${target_name} PRIVATE ${private_headers} ${source_files})
-    endif()
-
-    # read submodules
-    if (EXISTS ${path}/modules)
-        _get_directories (${path}/modules modules)
-
-        foreach (mod ${modules})
-            add_library_dir (${mod} STATIC)
-            get_filename_component (mod_name ${mod} NAME)
-            
-            target_link_libraries (${target_name} PRIVATE ${mod_name})
+        foreach (dir ${TREEMAKE_PUBLIC_DIR})
+            target_include_directories (${target_name} PUBLIC ${path}/${dir})
         endforeach()
     endif()
 
+    # read source files and private include folder
+    _find_in_dirs (
+            GLOB_RECURSE
+            "${TREEMAKE_HEADER_EXT}"
+            ${path}
+            "${TREEMAKE_PRIVATE_DIR}"
+            source_files)
+
+    _find_in_dirs (
+            GLOB_RECURSE
+            "${TREEMAKE_SOURCE_EXT}"
+            ${path}
+            "${TREEMAKE_PRIVATE_DIR}"
+            source_files)
+
+    list(LENGTH source_files source_file_count)
+
+    if (NOT source_file_count EQUAL 0)
+        target_sources (${target_name} PUBLIC ${source_files})
+
+        foreach (dir ${TREEMAKE_PRIVATE_DIR})
+            target_include_directories (${target_name} PUBLIC ${path}/${dir})
+        endforeach()
+    endif()
+
+    # read submodules
+    foreach(dir ${TREEMAKE_MODULE_DIR})
+        set (module_path ${path}/${dir})
+
+        if (EXISTS ${module_path})
+            _get_directories (${module_path} modules)
+
+            foreach (mod ${modules})
+                add_library_dir (${mod} STATIC)
+                get_filename_component (mod_name ${mod} NAME)
+
+                target_link_libraries (${target_name} PRIVATE ${mod_name})
+            endforeach()
+        endif()
+    endforeach ()
+
     # read tests
-    if (EXISTS ${path}/test)
-        _add_target_dir_test (${target_name} ${path})
+    if (TREEMAKE_ENABLE_TESTING)
+        foreach (dir ${TREEMAKE_TEST_DIR})
+            set (test_path ${path}/${dir})
+
+            if (EXISTS ${test_path})
+                _add_target_dir_test (${target_name} ${test_path})
+            endif()
+        endforeach ()
     endif()
 
     # include cmakelists
@@ -196,7 +245,7 @@ function (add_executable_dir directory)
 
     add_executable(${target_name})
 
-    _glob_sources (GLOB ${path} source_files)
+    _find_in_path (GLOB "${TREEMAKE_SOURCE_EXT}" ${path} source_files)
     list(LENGTH source_files source_files_count)
 
     if (source_files_count EQUAL 0)
@@ -214,7 +263,7 @@ function (add_library_dir directory target_type)
 
     add_library (${target_name} ${target_type})
 
-    _glob_sources (GLOB ${path} source_files)
+    _find_in_path (GLOB "${TREEMAKE_SOURCE_EXT}" ${path} source_files)
     list(LENGTH source_files source_files_count)
 
     if (source_files_count EQUAL 0)
